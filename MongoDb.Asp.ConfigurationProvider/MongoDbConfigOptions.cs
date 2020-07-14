628b77e8e2b173568e7ff751f817971a43b439a1
yadavvineet <code@vineetyadav.com>
2020-07-15 03:52:26 +0530

    documentation added for factory methods

--- MongoDb.Asp.ConfigurationProvider/MongoDbConfigOptions.cs
+++ MongoDb.Asp.ConfigurationProvider/MongoDbConfigOptions.cs
@@ -28,14 +28,40 @@
             ReadOption = ConfigReadOption.DefinedKeys;
         }
 
+        /// <summary>
+        /// Creates the default configuration to read from a given mongo db collection for ALL Keys. It will traverse all documents to extract keys. Keys can be OVERWRITTEN
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <returns>MongoDbConfigOptions.</returns>
         public static MongoDbConfigOptions GetOptionsForAllKeysAllDocuments(string connectionString, string databaseName, string collectionName)
         {
             return new MongoDbConfigOptions(connectionString, databaseName, collectionName);
         }
+
+        /// <summary>
+        /// Creates the default configuration to read from a given mongo db collection for DEFINED Keys. It will traverse all documents to extract keys. Keys can be OVERWRITTEN
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <param name="keysToRead">The keys to read.</param>
+        /// <returns>MongoDbConfigOptions.</returns>
         public static MongoDbConfigOptions GetOptionsForDefinedKeysAllDocuments(string connectionString, string databaseName, string collectionName, params string[] keysToRead)
         {
             return new MongoDbConfigOptions(connectionString, databaseName, collectionName, keysToRead);
         }
+
+        /// <summary>
+        /// Creates the default configuration to read from a given mongo db collection for ALL Keys  additionally applying a filter to fetch records. Keys can be OVERWRITTEN if multiple records are found for the filter.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <param name="keyToQuery">The key to query.</param>
+        /// <param name="valueToMatch">The value to match.</param>
+        /// <returns>MongoDbConfigOptions.</returns>
         public static MongoDbConfigOptions GetOptionsForAllKeysFilteredDocuments(string connectionString, string databaseName,
             string collectionName, string keyToQuery, object valueToMatch)
         {
@@ -47,6 +73,16 @@
             };
             return instance;
         }
+        /// <summary>
+        /// Creates the default configuration to read from a given mongo db collection for DEFINED Keys additionally applying a filter to fetch records.Keys can be OVERWRITTEN if multiple records are found for the filter.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <param name="databaseName">Name of the database.</param>
+        /// <param name="collectionName">Name of the collection.</param>
+        /// <param name="keyToQuery">The key to query.</param>
+        /// <param name="valueToMatch">The value to match.</param>
+        /// <param name="keysToRead">The keys to read.</param>
+        /// <returns>MongoDbConfigOptions.</returns>
         public static MongoDbConfigOptions GetOptionsForDefinedKeysFilteredDocument(string connectionString, string databaseName,
             string collectionName, string keyToQuery, object valueToMatch, params string[] keysToRead)
         {
