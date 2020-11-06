fad24cf3ca0c1be0dff8304c10be5fb7b76de9f5
Luca Pisano <luca@lucapisano.it>
2020-11-06 12:47:47 +0100

    added support for nesed sections and lists

--- MongoDb.Asp.ConfigurationProvider/MongoDbConfigurationProvider.cs
+++ MongoDb.Asp.ConfigurationProvider/MongoDbConfigurationProvider.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Primitives;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace MongoDb.Asp.ConfigurationProvider
@@ -67,7 +69,7 @@ namespace MongoDb.Asp.ConfigurationProvider
             _collectionToUse = options.CollectionName;
             _readMode = options.ReadOption;
             _keysToRead = options.KeysToRead;
-            _itemsCollection = new Dictionary<string, string>();
+            _itemsCollection = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
             _runInQueryMode = options.QueryInFilteredMode;
             _keyToMatch = options.KeyToQuery;
             _valueToMatch = options.ValueToMatch;
@@ -110,7 +112,40 @@ namespace MongoDb.Asp.ConfigurationProvider
         {
             return _token;
         }
-
+        public IDictionary<string, string> GetData(BsonDocument doc)
+        {
+            var json = doc.ToJson();
+            var o = JsonConfigurationFileParser.Parse(GenerateStreamFromString(json));
+            return o;
+        }
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+        void AddDocumentToDictionary(BsonDocument document)
+        {
+            document.Remove("_id");
+            if (_readMode == ConfigReadOption.DefinedKeys && _keysToRead.Length > 0)
+            {
+                //remove any key listed in _keysToRead
+                foreach (var name in document.Names.ToList())
+                {
+                    if (!_keysToRead.Any(b => b.Equals(name, StringComparison.InvariantCultureIgnoreCase)))
+                    {
+                        document.Remove(name);
+                    }
+                }
+            }
+            foreach (var item in GetData(document))
+            {
+                _itemsCollection.Add(item.Key, item.Value);
+            }
+        }
         /// <summary>
         /// Loads configuration values from the source represented by this <see cref="T:Microsoft.Extensions.Configuration.IConfigurationProvider" />.
         /// </summary>
@@ -127,39 +162,14 @@ namespace MongoDb.Asp.ConfigurationProvider
             {
                 foreach (var document in configItemList)
                 {
-                    foreach (var item in document.Names)
-                    {
-                        if (item.Equals("_id", StringComparison.InvariantCultureIgnoreCase)) continue;
-
-                        if (_itemsCollection.ContainsKey(item))
-                        {
-                            _itemsCollection[item] = GetStringValue(document.GetValue(item));
-                        }
-                        else
-                        {
-                            _itemsCollection.Add(item, GetStringValue(document.GetValue(item)));
-                        }
-                    }
+                    AddDocumentToDictionary(document);
                 }
             }
             else if (_readMode == ConfigReadOption.DefinedKeys && _keysToRead.Length > 0)
             {
                 foreach (var document in configItemList)
                 {
-                    foreach (var item in document.Names)
-                    {
-                        if (_keysToRead.Any(b => b.Equals(item, StringComparison.InvariantCultureIgnoreCase)))
-                        {
-                            if (_itemsCollection.ContainsKey(item))
-                            {
-                                _itemsCollection[item] = GetStringValue(document.GetValue(item));
-                            }
-                            else
-                            {
-                                _itemsCollection.Add(item, GetStringValue(document.GetValue(item)));
-                            }
-                        }
-                    }
+                    AddDocumentToDictionary(document);
                 }
             }
         }
@@ -176,16 +186,28 @@ namespace MongoDb.Asp.ConfigurationProvider
         }
 
         /// <summary>
-        /// Returns the immediate descendant configuration keys for a given parent path based on this
-        /// <see cref="T:Microsoft.Extensions.Configuration.IConfigurationProvider" />s data and the set of keys returned by all the preceding
-        /// <see cref="T:Microsoft.Extensions.Configuration.IConfigurationProvider" />s.
+        /// Returns the list of keys that this provider has.
         /// </summary>
-        /// <param name="earlierKeys">The child keys returned by the preceding providers for the same parent path.</param>
-        /// <param name="parentPath">The parent path.</param>
-        /// <returns>The child keys.</returns>
-        public IEnumerable<string> GetChildKeys(IEnumerable<string> earlierKeys, string parentPath)
+        /// <param name="earlierKeys">The earlier keys that other providers contain.</param>
+        /// <param name="parentPath">The path for the parent IConfiguration.</param>
+        /// <returns>The list of keys for this provider.</returns>
+        public virtual IEnumerable<string> GetChildKeys(
+            IEnumerable<string> earlierKeys,
+            string parentPath)
         {
-            return earlierKeys;
+            var prefix = parentPath == null ? string.Empty : parentPath + ConfigurationPath.KeyDelimiter;
+
+            return _itemsCollection
+                .Where(kv => kv.Key.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                .Select(kv => Segment(kv.Key, prefix.Length))
+                .Concat(earlierKeys)
+                .OrderBy(k => k, ConfigurationKeyComparer.Instance);
+        }
+
+        private static string Segment(string key, int prefixLength)
+        {
+            var indexOf = key.IndexOf(ConfigurationPath.KeyDelimiter, prefixLength, StringComparison.OrdinalIgnoreCase);
+            return indexOf < 0 ? key.Substring(prefixLength) : key.Substring(prefixLength, indexOf - prefixLength);
         }
     }
 }
